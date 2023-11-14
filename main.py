5a4794833760b746f86597f271b1e4f41232fe42
foilest-123 <thegreatvijay1991@gmail.com>
2023-11-14 13:30:30 +0530

    change function

--- main.py
+++ main.py
@@ -2,22 +2,23 @@ from pathlib import Path
 from google.cloud import storage
 
 
-bucket_name = "json_file_folder"
-source_folder = r"./json_data"
+def upload_files_to_gcs():
+    
+    bucket_name = "json_file_folder"
+    source_folder = r"./json_data"
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    
+    source_folder_path = Path(source_folder)
+    my_files = [file for file in source_folder_path.iterdir() if file.is_file()]
 
-storage_client = storage.Client()
-bucket = storage_client.get_bucket(bucket_name)
+    for file_path in my_files:
+        filename = file_path.name
+        destination_blob_name = filename
+        blob = bucket.blob(destination_blob_name)
+        blob.upload_from_filename(str(file_path))
 
-source_folder_path = Path(source_folder)
-my_files = [file for file in source_folder_path.iterdir() if file.is_file()]
-
-for file_path in my_files:
-    filename = file_path.name
-    destination_blob_name = filename
-    blob = bucket.blob(destination_blob_name)
-    blob.upload_from_filename(str(file_path))
-
-    print(f"{filename} uploaded to blob as {destination_blob_name}.")
+        print(f"{filename} uploaded to blob as {destination_blob_name}.")
 
 
 
